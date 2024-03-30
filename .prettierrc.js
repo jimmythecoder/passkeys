93f61a3b91b96abbc875eed268870f8a9095de71
James Harris <dev@jharris.nz>
2024-03-31 00:33:06 +1100

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,10 @@
 export default {
     tabWidth: 4,
     printWidth: 150,
+    singleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    semi: true,
+    useTabs: false,
+    arrowParens: "always",
 };
